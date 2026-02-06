5cb3432a4dd18c09d53e10a276437fa97882ce54
romini1993 <83305908+romini1993@users.noreply.github.com>
2026-02-06 15:13:57 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,11 @@
-const CACHE_NAME = 'kurs-precision-v1';
-const ASSETS_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.json'
-];
-
-// Install Service Worker & Simpan Cache
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('kurs-v1').then((cache) => cache.addAll(['./', './index.html', './manifest.json']))
   );
 });
 
-// Strategi: Ambil dari internet dulu, kalau gagal (offline) baru ambil dari cache
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
-    })
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
